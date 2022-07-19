4a40e2bfc7a07684b8a67e66bf607498fa8704f1
mpanius <mpanius@users.noreply.github.com>
2022-07-19 07:44:03 +0000

    Fix styling

--- src/LaravelJitsuServiceProvider.php
+++ src/LaravelJitsuServiceProvider.php
@@ -7,8 +7,6 @@ use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 class LaravelJitsuServiceProvider extends PackageServiceProvider
 {
-
-
     public function configurePackage(Package $package): void
     {
         /*
@@ -20,6 +18,4 @@ class LaravelJitsuServiceProvider extends PackageServiceProvider
             ->name('laravel-jitsu')
             ->hasConfigFile();
     }
-
-
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!function_exists('jitsu')) {
+if (! function_exists('jitsu')) {
     function jitsu(array $data = [], string $event_name = 'event', string $api_key_name = 'default')
     {
         return \Mpanius\LaravelJitsu\LaravelJitsu::push($data, $event_name, $api_key_name);
